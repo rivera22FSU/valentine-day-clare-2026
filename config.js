53a1a2772b76b30cde6b18eec3173e13ebe68dbf
rivera22FSU <rivera22@ferris.edu>
2026-02-12 23:48:09 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://soundcloud.com/oliviaadean/ive-seen-it", // Music streaming URL
+        musicUrl: "https://screenapp.io/app/#/library/69894f8990e226c3894467ac/default/5e303d25-4e0b-44cb-bf7c-950505127465", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
